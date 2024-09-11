50ea764534afcce29f6ac527d9971c6ff1a5458b
proabdulahad <144206282+proabdulahad@users.noreply.github.com>
2024-09-11 23:28:36 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'sans': ['Helvetica', 'Arial', 'sans-serif'],
+          'serif': ['Georgia', 'Cambria', 'serif'],
+          'mono': ['Menlo', 'Monaco', 'monospace'],
+          'title': ["Alegreya"],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
